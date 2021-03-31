24ea637100c6ed9ccaf78c397468b98001bab688
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-03-31 15:52:16 -0300

    first commit

---
+++ src/config/sequelizefile.js
@@ -0,0 +1,12 @@
+//require('dotenv').config()
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '12345',
+    database: 'projeto_docum',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+}
