5c7644e97629866daecb97ccb851bb974ec6676c
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-03-31 22:13:10 -0300

    CONFIGURAÇOES DO BANCO

--- src/server.js
+++ src/server.js
@@ -14,5 +14,11 @@ app.disable('x-powered-by') // Remover da resposta HTTP (header: x-powered-by) a
 app.listen(process.env.PORT || 3000, () => {
     console.log('Executando...') 
     console.log('Testar Banco: ' + process.env.DB_BANCO)
+    console.log('Testar HOST: ' + process.env.DB_HOST)
+    console.log('Testar USER: ' + process.env.DB_USER)
+    console.log('Testar SENHA: ' + process.env.DB_PASSWORD)
+    console.log('Testar PORTA: ' + process.env.DB_PORT)
+    console.log('Testar AUTH SECRET: ' + process.env.AUTH_SECRET)
+    console.log('Testar URL: ' + process.env.DATABASE_URL)
 })
 
