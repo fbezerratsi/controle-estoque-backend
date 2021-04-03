aa9beb0988d568fd6f8a795c91f1fc4837495e8b
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-04-03 11:43:06 -0300

    atualizar swagger 4

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = express()
 const routes = require('./routes')
 const swaggerUI = require('swagger-ui-express')
 const swaggerJsDoc = require('swagger-jsdoc')
+const cors = require('cors')
 
 dotenv.config()
 
@@ -30,6 +31,7 @@ const specs = swaggerJsDoc(options)
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.disable('x-powered-by') // Remover da resposta HTTP (header: x-powered-by) a referência de que o Express/Node compõem a lista de tecnologias utilizadas, isso irá afastar rotinas mais simples de varredura e ataques automatizados
